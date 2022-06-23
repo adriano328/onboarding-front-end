93edcd57d5851ddd5c0ef11896c04fa17af6646f
adriano328 <lucasadrianodias@gmail.com>
2022-06-22 21:25:55 -0400

    Envio de primeiros dados

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {CardModule} from 'primeng/card';
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import { HomePageComponent } from './modules/home/home-page/home-page.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePageComponent
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CardModule,
+    InputTextModule,
+    ButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
