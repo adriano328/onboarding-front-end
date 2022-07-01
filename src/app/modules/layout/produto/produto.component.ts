486c2747e825d41abf2de658e1c0a022d72bbca9
adriano328 <lucasadrianodias@gmail.com>
2022-07-01 17:28:33 -0400

    implementação dropdown classe produto

--- src/app/modules/layout/produto/produto.component.ts
+++ src/app/modules/layout/produto/produto.component.ts
@@ -1,12 +1,14 @@
 import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { ICategoria } from 'src/app/interface/ICategoria';
 import { IProduto } from 'src/app/interface/IProduto';
+import { CategoriaService } from 'src/app/services/categoria.service';
 import { ProdutoService } from 'src/app/services/produto.service';
 @Component({
   selector: 'app-produto',
   templateUrl: './produto.component.html',
   styleUrls: ['./produto.component.scss'],
-  providers: [ProdutoService]
+  providers: [ProdutoService, CategoriaService]
 
 })
 export class ProdutoComponent implements OnInit {
@@ -15,8 +17,13 @@ produtoList: IProduto = {} as IProduto
 
 listProduto: IProduto[] = [];
 
+listCategoria: ICategoria[] = [];
+
+selectedCat!: ICategoria;
+
   constructor(
-  private produtoService: ProdutoService
+  private produtoService: ProdutoService,
+  private categoriaService: CategoriaService
 
   ) {
     this.produtoService.findAll().then(sucess =>{
@@ -25,7 +32,13 @@ listProduto: IProduto[] = [];
   }
 
   ngOnInit(): void {
-
+    this.categoriaService.findAll().then(success => {
+      console.log(success);
+      
+      this.listCategoria = success!;
+    });    
   }
 
 }
+
+
